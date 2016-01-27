150e18fc99f524b45bd3b78ca8fbc5be2a6a61fb
Fran Thinway <thinway@icloud.com>
2016-01-27 13:28:15 +0100

    Estructura de archivos inicial.

---
+++ home/home.html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Página de Prácticas</title>
+</head>
+<body>
+	<h1>Página de prácticas</h1>
+	<a href="login">Login</a>
+	<!-- Enlaces que deben aparecer cuando el usuario esté logueado
+	     con las sesión creada 
+	<a href="upload">Subir práctica</a> - 
+	<a href="logout">Salir</a> -->
+	<ul>
+		<li>#1 - ejercicio.docx</li>
+		<li>#2 - practica.html</li>
+		<li>#3 - tarea.pdf</li>
+		<li>#4 - trabajo.jgp</li>
+		<li>#5 - practica.odt</li>
+	</ul>
+</body>
+</html>
